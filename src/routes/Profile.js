3faa102d35643034ada0f03fdf565612c9fba442
ryun <run2070@naver.com>
2022-04-06 18:28:50 +0900

    profile컴포넌트 추가 및 로그아웃 기능 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,22 @@
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
-export default () => <span>profile</span>
+const Profile = () => {
+
+    const navigate = useNavigate();
+
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate("/");
+    }
+
+    return (
+        <div>
+            <span>profile</span>
+            <button onClick={onLogOutClick} Link="/">log out</button>
+        </div>
+    )
+}
+
+export default Profile;
