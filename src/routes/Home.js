f85cf6efce3ddb0386b2edc83639ef1f3b7477d8
ryun <run2070@naver.com>
2022-04-11 18:37:55 +0900

    파일 업로드 기능 추가 작업중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,6 +9,8 @@ const Home = (userObj) =>{
     const [reacts, setReact] = useState("");
     //firebase database에 있는 텍스트 리스트
     const [reactArr, setReactArr] = useState([]);
+    //file upload
+    const [attachment, setAttachment] = useState();
 
     // const getReacts = async() =>{
     //     const dbReacts = await dbService.collection("reacts").get();
@@ -60,11 +62,57 @@ const Home = (userObj) =>{
 
         setReact(value);
     }
+
+    //setImage
+    const onFileChange = (event) => {
+        
+        const {
+            target : {files}
+        } = event;
+
+        const theFile = files[0];
+
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result }
+            } = finishedEvent
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile)
+
+    }
+
+    //이미지 초기화
+    const onClearAttachmentClick = () => {
+        setAttachment(null)
+    }
+
     return (
         <div>
             <form>
-                <input value={reacts} onChange={onChange} type="text" placeholder="what's on your mind?" required maxLength={120}></input>
-                <input type="submit" onClick={onSubmit} value="react"></input>
+                <input 
+                    value={reacts} 
+                    onChange={onChange} 
+                    type="text" 
+                    placeholder="what's on your mind?" 
+                    required maxLength={120}
+                />
+                <input type="file" accept="image/*" onChange={onFileChange}/>
+                <input 
+                    type="submit" 
+                    onClick={onSubmit} 
+                    value="react"
+                />
+                {attachment &&(
+                    <div>
+                        <img src={attachment} witdh="50px" height="50px"/>
+                        <button onClick={onClearAttachmentClick}>clear</button>
+                    </div>
+                    
+                    )
+                }
+                
             </form>
             <div>
                 {reactArr.map((react)=>(
